e854c199e3a89ba52582e5b58cd1104c39907bee
Надежда Кузнецова <kuznecovanadezhd@gmail.com>
2022-01-17 22:49:31 +0300

    Добавьте файлы проекта.

---
+++ Lab16.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Lab16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product maxProduct = products[0];
+            foreach (Product e in products)
+            {
+                if (e.Summa>maxProduct.Summa)
+                {
+
+                    maxProduct = e;
+                }
+                Console.WriteLine($"{maxProduct.Code} {maxProduct.Name} {maxProduct.Summa}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
